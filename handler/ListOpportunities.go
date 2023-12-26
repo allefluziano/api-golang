03f45222ad76e4e45a53d553340355427b5d07f5
allefluziano <allefluziano@gmail.com>
2023-12-26 15:26:07 -0300

    feat [get] - add show and list opportunities

--- handler/ListOpportunities.go
+++ handler/ListOpportunities.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/allefluziano/api-golang/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpportunitiesHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opportunity",
-	})
+	opportunities := []schemas.Opportunity{}
+
+	if err := db.Find(&opportunities).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error list opportunities")
+		return
+	}
+
+	sendSuccess(ctx, "list-opportunities", opportunities)
 }
--- handler/showOpportunity.go
+++ handler/showOpportunity.go
@@ -1,13 +1,27 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/allefluziano/api-golang/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpportunityHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opportunity",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id",
+			"queryParameter").Error())
+		return
+	}
+
+	opportunity := schemas.Opportunity{}
+
+	if err := db.First(&opportunity, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opportunity with id: %s not found", id))
+		return
+	}
+
+	sendSuccess(ctx, "show-opportunity", opportunity)
 }
