aed8fc2f3c4cdbefe69b318cd0fa9f104c4723ff
allefluziano <allefluziano@gmail.com>
2023-12-26 11:42:18 -0300

    feat [router] - add subpackage

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/allefluziano/api-golang/router"
+
+func main() {
+
+	// Initialize Router
+	router.Initialize()
+}
---
+++ router/router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Initialize() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":8080")
+}
