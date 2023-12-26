e9478bfc40470d8b7a2caad93c0a5f11c8680f68
allefluziano <allefluziano@gmail.com>
2023-12-26 12:50:28 -0300

    feat [logger] - add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/allefluziano/api-golang/router"
+import (
+	"github.com/allefluziano/api-golang/config"
+	"github.com/allefluziano/api-golang/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
 	// Initialize Router
 	router.Initialize()
